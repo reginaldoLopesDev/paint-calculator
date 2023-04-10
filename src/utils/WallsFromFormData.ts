8ce22663a36463ef35e937fb17f674534755f4d0
Reginaldo Lopes <reginaldo.lopes.dev@gmail.com>
2023-04-10 03:05:53 -0300

    added: util method

---
+++ src/utils/WallsFromFormData.ts
@@ -0,0 +1,16 @@
+import { Wall } from '../Wall';
+
+export function createWallsFromFormData(formData: any): Wall[] {
+  const walls: Wall[] = [];
+
+  for (let i = 1; i <= 4; i++) {
+    const height = formData[`wall${i}Height`];
+    const width = formData[`wall${i}Width`];
+    const windows = formData[`wall${i}Windows`];
+    const doors = formData[`wall${i}Doors`];
+
+    walls.push(new Wall(height, width, windows, doors));
+  }
+
+  return walls;
+}
